3bfe20453aba8ad1044fa9bcfba6dccd5978b16c
jana_jo <Joanna.Janas@dlr.de>
2016-07-27 17:11:09 +0200

    Add missing test_files

---
+++ tests/test_config_dict.py
@@ -0,0 +1,11 @@
+import numpy as np
+from datetime import datetime
+
+
+test_config = {'Processing': {'logging': False},
+               'Data': {'dark':'../../measurements/Ostsee/T4/ST06/darkcurrent000.asc',
+                        'utc_time': datetime(2016, 4, 14, 8, 47),
+                        'gps_coords': np.array([ 53.9453236, 11.3829424,   0.]),
+                        'target': '../../measurements/Ostsee/T4/ST06/target000.asc',
+                        'reference': '../../measurements/Ostsee/T4/ST06/reference000.asc'}}
+
