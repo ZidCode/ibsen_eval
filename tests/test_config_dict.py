6beb6d1cd7f8e84e42e0b0c506975c30ab6334c7
jana_jo <Joanna.Janas@dlr.de>
2016-08-18 17:25:43 +0200

    Fix commit c6a771f8

--- tests/test_config_dict.py
+++ tests/test_config_dict.py
@@ -2,7 +2,7 @@ import numpy as np
 from datetime import datetime
 
 
-test_config = {'Processing': {'logging': False},
+test_config = {'Processing': {'logging': False, 'logging_level': 'INFO'},
                'Data': {'dark':'../../measurements/Ostsee/T4/ST06/darkcurrent000.asc',
                         'utc_time': datetime(2016, 4, 14, 8, 47),
                         'gps_coords': np.array([ 53.9453236, 11.3829424,   0.]),
