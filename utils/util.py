20dcb4914e8652e6a2b1a305600c7ab1687011b1
zidcode <zidcode@googlemail.com>
2016-08-19 13:37:09 +0200

    Add missing util package

---
+++ utils/util.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def construct_weights(x, range_):
+
+    weights = np.ones(len(x))
+    index_ = np.concatenate((np.where(x < min(range_))[0], np.where(x > max(range_))[0]))
+    print(index_)
+    weights[index_] = 0
+    return weights
