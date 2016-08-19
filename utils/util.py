86a882c088b0b38dfebea1507405d235df49270e
zidcode <zidcode@googlemail.com>
2016-08-19 13:49:32 +0200

    fixup weights

--- utils/util.py
+++ utils/util.py
@@ -5,6 +5,5 @@ def construct_weights(x, range_):
 
     weights = np.ones(len(x))
     index_ = np.concatenate((np.where(x < min(range_))[0], np.where(x > max(range_))[0]))
-    print(index_)
     weights[index_] = 0
     return weights
