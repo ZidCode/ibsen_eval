b5dfcecab139a3176a5b8fee8ffd3cdd95b4b176
jana_jo <Joanna.Janas@dlr.de>
2016-10-17 15:41:43 +0200

    calibration: Describe usage in .py

--- calibration/ibsen_calibration.py
+++ calibration/ibsen_calibration.py
@@ -77,6 +77,10 @@ def generate_ibsen_calibration_files(directory, reference):
 
 
 if __name__ == "__main__":
+    """Usage:
+        python ibsen_calibration.py -d /users/jana_jo/DLR/Codes/calibration/Ibsen_0109_5313264/EOC/Optiklabor/
+    """
+
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--directory', help="Add directory with raw data measured by Rasta")
