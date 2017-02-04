55997bf3b0768893aca598eaf913e0d32bbcfc01
zidcode <zidcode@googlemail.com>
2017-02-04 17:54:10 +0100

    parser: Update AeronetParser

--- parser/aeronet.py
+++ parser/aeronet.py
@@ -5,24 +5,32 @@ import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
 from lmfit import Model
+import ConfigParser
 from utils.plotting import plot_turbidity
+from ast import literal_eval
+
+
+def parse_aeronet_config(ini_file):
+    convert_to_array = lambda x, m : np.array([m(s) for s in x.split(',')])
+    config = ConfigParser.ConfigParser()
+    config.read(ini_file)
+    config_dict = {s: dict(config.items(s)) for s in config.sections()}
+    config_dict['Data']['aod_range'] = convert_to_array(config_dict['Data']['aod_range'], int)
+    return config_dict['Data']
 
 
 class AeronetParser:
 
     def __init__(self):
+        self.reference = 550
         self.aeronet_dict = dict()
         self.convert2datetime = lambda d: datetime.strptime(d, '%d:%m:%Y %H:%M:%S')
         self.merge = lambda tup: tup[0] + ' ' + tup[1]
         self.padding = None
         self.timeline = None
         self.column_names = None
-        self.logger = logging.getLogger('Aeronet')
-        ch = logging.StreamHandler()
-        formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s - %(funcName)s')
-        ch.setFormatter(formatter)
-        self.logger.addHandler(ch)
-        self.logger.setLevel('INFO')
+        self.exact_wavelength_columns = None
+        self.logger = self._create_logger()
 
     def get_Column_names(self):
         return self.column_names
@@ -38,11 +46,13 @@ class AeronetParser:
         7: Columns
         8:end - Data
         """
-        self.reference = 550  # reference wavelength for turbidity calculation
+        # reference wavelength for turbidity calculation
         self.logger.info("Parsing %s" % aeronet_file)
-        self.padding = padding
+        self.padding = np.array([str(i) for i in range(padding)])
         frame = pd.read_csv(aeronet_file, header=skip_head, nrows=rows)
-        self.column_names = np.concatenate((np.array(frame.columns), np.array([str(i) for i in range(padding)])))
+        columns = np.array(frame.columns)
+        self.column_names = np.concatenate((columns, self.padding))
+        self.exact_wavelength_columns = np.concatenate(([columns[-1]], self.padding))
         data = np.genfromtxt(aeronet_file, skip_header=7, delimiter=',', dtype=str)
         for idx, column in enumerate(self.column_names):
             self.aeronet_dict[column] = data[:, idx]
@@ -68,62 +78,94 @@ class AeronetParser:
         if key:
             print('%s' % self.aeronet_dict[key])
         else:
-            for column in self.column_names[:-self.padding]:
+            for column in self.column_names:
                 print("%s" % column)
 
-    def get_turbidity(self):
-        import matplotlib.pyplot as plt
-        nm_in_strings = -2
-        AOD_names = [aod for aod in self.aeronet_dict.keys() if aod.startswith('AOD')]
-        lambdas = np.array([lam.split('_')[-1][:nm_in_strings] for lam in AOD_names])
+    def get_turbidity(self, aod_range):
+
         self.aeronet_dict['Turbidity'] = np.zeros(len(self.timeline))
         self.aeronet_dict['Turbidity_stderror'] = np.zeros(len(self.timeline))
+
+        wave = self._get_exact_lambdas()
+        AOD_names = self._delete_invalid_aods()
+        assert len(AOD_names) == len(wave)
+        self.logger.info("Lambdas %s \n AODs: %s" % (wave, AOD_names))
+        wave, AOD_names = self._cut_range(aod_range, wave, AOD_names)
+        self.logger.info("Used Lambdas %s \n \t \t Used AODs: %s" % (wave, AOD_names))
+        angstrom_exponent_column = self._get_angstrom_columnname(aod_range)
+        self.logger.info("Aengstrom Column %s" % angstrom_exponent_column)
         for idx, time in enumerate(self.timeline):
-            aods_str = np.array([self.aeronet_dict[aod_name][idx] for aod_name in AOD_names])
-            _idx = np.where(aods_str != '-999.000000')[0]
-            wave_available = np.array(map(float, lambdas[_idx]))
-            aods = np.array(map(float, aods_str[_idx]))
-            sort_idx = wave_available.argsort()
-            wave = wave_available[sort_idx][1:-3]  # magic numbers... to lazy right now to change this
-            aods = aods[sort_idx][1:-3]  # magic numbers... to lazy right now to change this
+            aods = np.array([float(self.aeronet_dict[key][idx]) for key in AOD_names])
+            self.alpha = float(self.aeronet_dict[angstrom_exponent_column][idx])
             result = self._fit_aengstrom(aods, wave)
             wave_new = np.linspace(wave[0], wave[-1], 1000)
-            fitted = self._aengstrom_formula(wave_new, result.params['alpha'].value, result.params['beta'].value)
+            fitted = self._aengstrom_formula(wave_new, result.params['beta'].value)
             #plt = plot_turbidity(wave, aods, wave_new, fitted)
             self.aeronet_dict['Turbidity'][idx] = result.params['beta'].value
             self.aeronet_dict['Turbidity_stderror'][idx] = result.params['beta'].stderr
-        self.logger.info("Available AODs %s " % sorted(wave_available))
-        self.logger.info("ATTENTION badly hardcoded ============== Used AODs %s" % wave)
+        #plt.show()
 
+    def _get_angstrom_columnname(self, aod_range):
+        return '%s-%s_Angstrom_Exponent' % (aod_range[0], aod_range[1])
 
-    def _aengstrom_formula(self, x, alpha, beta):
-        return beta * (x / self.reference) ** (alpha)
+    def _aengstrom_formula(self, x, beta):
+        return beta * (x / self.reference) ** (-self.alpha)
 
     def _fit_aengstrom(self, Aods, lambdas):
-        gmod = Model(self._aengstrom_formula, independent_vars=['x'], param_names=['alpha', 'beta'])
+        gmod = Model(self._aengstrom_formula, independent_vars=['x'], param_names=['beta'])
         gmod.set_param_hint('beta', value=0.1)
-        gmod.set_param_hint('alpha', value=1.5)
-        result = gmod.fit(Aods, x=lambdas)
+        result = gmod.fit(Aods, x=lambdas, verbose=False)
         return result
 
+    def _get_exact_lambdas(self):
+        return np.array(sorted([float(self.aeronet_dict[col][0]) * 1000 for col in self.exact_wavelength_columns]))
+
+    def _delete_invalid_aods(self):
+        nm_in_strings = -2
+        AOD_names = np.array([aod for aod in self.aeronet_dict.keys() if aod.startswith('AOD')])
+        lambdas = np.array([lam.split('_')[-1][:nm_in_strings] for lam in AOD_names])
+        aods_str = np.array([self.aeronet_dict[aod_name][0] for aod_name in AOD_names])
+        _idx = np.where(aods_str != '-999.000000')[0]
+        lambdas = np.array(map(float, lambdas[_idx]))
+        AOD_names = AOD_names[_idx]
+        sort_idx = lambdas.argsort()
+        return AOD_names[sort_idx]
+
+    def _cut_range(self, aod_range, wave, AOD_names):
+        start = 'AOD_%snm' % aod_range[0]
+        end = 'AOD_%snm' % aod_range[1]
+        idx_start = np.where(AOD_names == start)[0][0]
+        idx_end = np.where(AOD_names == end)[0][0] + 1
+        return wave[idx_start:idx_end], AOD_names[idx_start:idx_end]
+
+    def _create_logger(self):
+        logger = logging.getLogger('Aeronet')
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s - %(funcName)s')
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+        logger.setLevel('INFO')
+        return logger
+
+
+def main(config_file):
+    config = parse_aeronet_config(config_file)
+
+    Parser = AeronetParser()
+    aeronaet = Parser.parse(config['source'])
+    time = Parser.get_Timeline()
+    #Parser.show('Solar_Zenith_Angle(Degrees)')
+    #Parser.pretty_print()
+    Parser.get_turbidity(config['aod_range'])
+    #plt.plot(time, aeronaet['500-870_Angstrom_Exponent'], '+', label='440-870')
+    plt.errorbar(time, aeronaet['Turbidity'], yerr=aeronaet['Turbidity_stderror'], ecolor='g', fmt=None)
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
     import argparse
-    default_file = '/home/jana_jo/DLR/Codes/measurements/Aeronet/20161128_20161130_Munich_University/20161129_Munich_University.lev15'
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', default=default_file)
+    parser.add_argument('-c', '--config', default='aeronet_config.ini')
     args = parser.parse_args()
-    Parser = AeronetParser()
-    aeronaet = Parser.parse(args.file)
-    time = Parser.get_Timeline()
-    #Parser.show('Solar_Zenith_Angle(Degrees)')
-    #Parser.pretty_print()
-    Parser.get_turbidity()
-    plt.plot(time, aeronaet['440-870_Angstrom_Exponent'], '+', label='440-870')
-    #plt.errorbar(time, aeronaet['Turbidity'], yerr=aeronaet['Turbidity_stderror'], ecolor='g', fmt=None)
-    plt.plot(time, aeronaet['380-500_Angstrom_Exponent'], '+', label='380-500')
-    plt.plot(time, aeronaet['440-675_Angstrom_Exponent'], '+', label='440-675')
-    plt.plot(time, aeronaet['500-870_Angstrom_Exponent'], '+', label='500-870')
-    plt.legend()
-    plt.show()
+    main(args.config)
