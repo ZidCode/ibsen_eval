be3deed5199729602530544854993ac3dd4aa9cc
jana_jo <Joanna.Janas@dlr.de>
2017-02-08 17:57:25 +0100

    Change Plot labels and beautify

--- utils/plotting.py
+++ utils/plotting.py
@@ -17,7 +17,7 @@ def plot_meas(tar, ref):
     gs = gridspec.GridSpec(2, 2)
     ax1 = plt.subplot(gs[0, :])
     ax2 = plt.subplot(gs[1, :])
-    map_dicts = {r'Radiance $L_{sky}$ Zenith': {'plt': ax1, 'meas': tar},
+    map_dicts = {r'Radiance $E_{ds}$ Zenith': {'plt': ax1, 'meas': tar},
                  r'Irradiance $E_d$': {'plt': ax2, 'meas': ref}}
 
     for k, v in map_dicts.items():
@@ -36,14 +36,15 @@ def plot_used_irradiance_and_reflectance(tarmd, refmd, reflectance):
     gs = gridspec.GridSpec(2, 2)
     ax1 = plt.subplot(gs[0, :])
     ax2 = plt.subplot(gs[1, :])
-    map_dict = {'L_sky_mean': tarmd, 'E_d_mean': refmd}
+    map_dict = {'E_ds_mean': tarmd, 'E_d_mean': refmd}
 
     for key, meas in map_dict.items():
         ax1.plot(meas['wave'], meas['data'], alpha=0.1)
         ax1.plot(meas['wave'], meas['mean'], label='%s' %key)
 
-    ax1.set_title(r'$L_{sky}$ and $E_{d}$', **hfont)
+    ax1.set_title(r'$E_{ds}$ and $E_{d}$', **hfont)
     ax2.plot(reflectance['wave_nm'], reflectance['spectra'], '+')
+    ax1.set_ylabel(r'$\frac{mW}{nm m^2}$', **hfont)
     ax2.set_ylabel(r'Ratio [$\%$]', **hfont)
     ax1.legend(loc='best', prop=fontP)
     plt.xlabel('Wavelength [nm]', **hfont)
@@ -65,8 +66,8 @@ def plot_fitted_reflectance(result, param_dict, measurement):
     ax2.plot(measurement['wave_nm'], measurement['spectra'])
     ax2.plot(param_dict['wave_range'], result.best_fit, 'r-')
     ax2.errorbar(param_dict['wave_range'], param_dict['spectra_range'], yerr=param_dict['std'], ecolor='g')
-    ax2.set_title('Fitted reflectance', **hfont)
-    ax2.set_ylabel('Reflectance', **hfont)
+    ax2.set_title('Fitted ratio', **hfont)
+    ax2.set_ylabel('Ratio', **hfont)
     ax2.set_xlabel(r'Wavelength $\left[nm\right]$', **hfont)
     plt.show()
 
@@ -118,7 +119,7 @@ def plot_aengstrom_parameters_aeronet(object_list, title):
     ax2.set_ylabel(r'Turbidity $\beta$', **hfont)
     ax3.set_ylabel(r'$g_{dsr}$', **hfont)
     ax4.set_ylabel(r'$g_{dsa}$', **hfont)
-    ax2.set_xlabel('Sun zenith $^{\circ}$', **hfont)
+    ax4.set_xlabel('UTC Times', **hfont)
     ax1.set_title('%s' % title, **hfont)
     plt.tight_layout()
     plt.show()
@@ -141,7 +142,7 @@ def aeronet_plot(aeronet, ax1, ax2, _, __):
     ax1.plot(aeronet['utc_times'], aeronet['440-675_Angstrom_Exponent'], '+', label='440-675')
     ax1.plot(aeronet['utc_times'], aeronet['500-870_Angstrom_Exponent'], '+', label='500-870')
     ax2.errorbar(aeronet['utc_times'], aeronet['Turbidity'], yerr=aeronet['Turbidity_stderror'],fmt='o',
-                 label='Aeronet', color='r', ecolor='r')
+                 label='Aeronet', color='r', ecolor='r', markersize='2')
 
     return ax1
 
