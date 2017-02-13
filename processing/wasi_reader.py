f0ba22aacda84c7a166f49a6c38cb39535f0f4db
jana_jo <Joanna.Janas@dlr.de>
2017-02-13 15:32:24 +0100

    processing: Enable wasi database reading

---
+++ processing/wasi_reader.py
@@ -0,0 +1,81 @@
+import numpy as np
+import os
+from matplotlib.font_manager import FontProperties
+
+
+FONTSTYLE = 'serif'
+FONTSIZE = 12
+hfont = {'family':FONTSTYLE, 'fontsize': FONTSIZE}
+fontP = FontProperties()
+fontP.set_family(FONTSTYLE)
+fontP.set_size('small')
+
+
+def wasi_oxygen(filename=os.path.dirname(os.path.realpath(__file__)) + '/WASI_database/O2.A'):
+    data = np.genfromtxt(filename, skip_header=4)
+    wave_nm = data[:,0]
+    O2 = data[:,1]
+    return wave_nm, O2
+
+
+def wasi_e0(filename=os.path.dirname(os.path.realpath(__file__)) + '/WASI_database/E0_sun.txt'):
+    data = np.genfromtxt(filename, skip_header=11)
+    wave_nm = data[:,0]
+    e0 = data[:,1]
+    return wave_nm, e0
+
+
+def wasi_wv(filename=os.path.dirname(os.path.realpath(__file__)) + '/WASI_database/WV.A'):
+    data = np.genfromtxt(filename, skip_header=4)
+    wave_nm = data[:,0]
+    wv = data[:,1]
+    return wave_nm, wv
+
+
+def wasi_ozone(filename=os.path.dirname(os.path.realpath(__file__)) + '/WASI_database/O3.A'):
+    data = np.genfromtxt(filename, skip_header=4)
+    wave_nm = data[:,0]
+    O3 = data[:,1]
+    return wave_nm, O3
+
+
+def get_wasi_parameters():
+    params = {'o2': wasi_oxygen, 'o3': wasi_ozone, 'e0': wasi_e0, 'wv': wasi_wv}
+    wasi_dict = dict()
+    for key, value in params.items():
+        wasi_dict[key] = dict()
+        wasi_dict[key]['wave'], wasi_dict[key]['values'] = value()
+    return wasi_dict
+
+
+def plot():
+    import matplotlib.pyplot as plt
+    wv_o2, o2 = wasi_oxygen()
+    wv_e0, e0 = wasi_e0()
+    wv_o3, o3 = wasi_ozone()
+    wv_wv, wv = wasi_wv()
+    plt.plot(wv_o2, o2, label=r'$O^2$')
+    plt.xlabel("Wavelength [nm]", **hfont)
+    plt.ylabel(r"mean absorption coefficient $\left[cm^{-1}\right]$", **hfont)
+    plt.legend()
+    plt.show()
+    plt.plot(wv_e0, e0, label=r'$E_0$')
+    plt.xlabel("Wavelength [nm]", **hfont)
+    plt.ylabel(r"mean extraterrestrial irradiance $\left[\frac{mW}{m^2\cdot nm}\right]$", **hfont)
+    plt.legend()
+    plt.show()
+    plt.plot(wv_o3, o3, label=r'$O^3$')
+    plt.xlabel("Wavelength [nm]", **hfont)
+    plt.ylabel(r"mean absorption coefficient $\left[cm^{-1}\right]$", **hfont)
+    plt.legend()
+    plt.show()
+    plt.plot(wv_wv, wv, label=r'Water Vapour')
+    plt.ylabel(r"mean absorption coefficient $\left[cm^{-1}\right]$", **hfont)
+    plt.xlabel("Wavelength [nm]", **hfont)
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot()
+    get_wasi_parameters()
