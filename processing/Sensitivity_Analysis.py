688fcaeeae608b6af1e8a68a1a563eb2e80409be
zidcode <zidcode@googlemail.com>
2017-03-07 09:55:07 +0100

    processing: Update Sensitivity
    + Success array
    + Handling of fixed values
    + Disable plots
    + Generate setups outside the scope

--- processing/Sensitivity_Analysis.py
+++ processing/Sensitivity_Analysis.py
@@ -25,53 +25,56 @@ def fit_setup():
     setup = dict()
     setup['variables'] = ['alpha', 'beta', 'l_dsr', 'l_dsa', 'H_oz', 'wv']
     setup['simulate']= [1.8, 0.06, 0.17, 0.1, 0.34, 1.2]
-    setup['expected']= [1.8, 0.06, 0.1, 1.2]
-    setup['guess'] = [1.5, 0.04, 0.09, 1.0]  # config
-    setup['bounds'] = [(-0.2, 4), (0., 3),(-0.05, 0.3), (-0.05, 3.3)]  # config
-    setup['model'] = SkyRadianceSym
-    setup['global'] = 'H_oz'
-    setup['local'] = 'l_dsr'
-    setup['independent'] = {'x':np.linspace(650, 750, 1000), setup['global']:0, setup['local']:0}
+    setup['expected']= [0.06, 0.1, 0.34]
+    setup['guess'] = [0.05,0.09, 0.3]  # config
+    setup['bounds'] = [(0., 3),(0.01, 0.7), (0.25, 0.7)]  # config
+    setup['model'] = SkyRadiance
+    setup['global'] = 'alpha'
+    setup['local'] = 'wv'
+    setup['independent'] = {'x':np.linspace(520, 650, 1000), setup['global']:0, setup['local']:0, 'l_dsr':0.17}
+    setup['dir'] = 'Lsky3/'
     return setup
 
 
 def sensi_setup():
     sensi = dict()
     sensi['statistics'] = 20
-    sensi['alpha'] = np.linspace(1.0, 2.5, 51)
-    sensi['beta'] = np.linspace(0.02, 0.13, 51)
-    sensi['l_dsr'] = np.linspace(0.12, 0.2, 31)
-    sensi['l_dsa'] = np.linspace(0.01, 0.1, 51)
-    sensi['g_dsa' ] = np.linspace(.3, .8, 51)
-    sensi['g_dsr'] = np.linspace(0.5, 1.0, 51)
-    sensi['H_oz'] = np.linspace(0.3, 0.5, 31)
-    sensi['wv'] = np.linspace(0.1, 2.4, 31)
+    sensi['alpha'] = np.linspace(1.0, 2.5, 31)
+    sensi['beta'] = np.linspace(0.02, 0.14, 31)
+    sensi['l_dsr'] = np.linspace(0.15, 0.21, 31)
+    sensi['l_dsa'] = np.linspace(0.01, 0.13, 31)
+    sensi['g_dsa' ] = np.linspace(.3, .8, 31)
+    sensi['g_dsr'] = np.linspace(0.5, 1.0, 31)
+    sensi['H_oz'] = np.linspace(0.3, 0.51, 31)
+    sensi['wv'] = np.linspace(0.1, 2.5, 31)
     return sensi
 
 
 get_index = lambda x, l: np.where(x == np.array(l))[0][0]
 
-def startsky2D(logger):
-    model_param = get_model_param()
-    setup = fit_setup()
-    sensi = sensi_setup()
-    r_setup = copy.copy(fit_setup())
+def startsky2D(model_param, setup, sensi):
+    r_setup = copy.copy(setup)
 
     model = BaseModelPython(model_param['zenith'], model_param['AMass'], model_param['pressure'], model_param['ssa'])
     skyModel = SkyRadiance(model)
     kwargs = {key:value for key, value in zip(setup['variables'], setup['simulate'])}
     kwargs['x'] = setup['independent']['x']
     simulation = skyModel.func(**kwargs)
-    import matplotlib.pyplot as plt
-    plt.plot(kwargs['x'], simulation)
-    plt.show()
+    # import matplotlib.pyplot as plt
+    # plt.plot(kwargs['x'], simulation)
+    # plt.show()
     del model
 
     global_var = {'param': setup['global'], 'input': sensi[setup['global']], 'idx':get_index(setup['global'], setup['variables'])}
     local_var = {'param': setup['local'], 'input': sensi[setup['local']],'idx':get_index(setup['local'], setup['variables'])}
 
-    r_setup['variables'].remove(r_setup['global'])
-    r_setup['variables'].remove(r_setup['local'])
+    for key in setup['independent'].keys():
+        if key == 'x':
+            # do nothing
+            pass
+        else:
+            r_setup['variables'].remove(key)
+
     fit_parameters = dict()
     for idx, key in enumerate(r_setup['variables']):
         fit_parameters[key] = dict()
@@ -126,7 +129,7 @@ def parallel_sensi(wq):
 
         model = BaseModelPython(job_params['model_param']['zenith'], job_params['model_param']['AMass'], job_params['model_param']['pressure'], job_params['model_param']['ssa'])
         skyModel = SkyRadiance(model)
-        callable = skyModel.func
+        callable_ = skyModel.func
 
         r_setup = job_params['r_setup']
         local_var = job_params['local_var']
@@ -140,12 +143,12 @@ def parallel_sensi(wq):
 
         for count in range(job_params['statistics']):
             noised_simulation = job_params['simulation'] + np.random.normal(0, 0.04, len(setup['independent']['x']))
-            result['output'] = _iterate(noised_simulation, r_setup['expected'], r_setup['guess'], r_setup['bounds'],
-                                        r_setup['variables'], r_setup['independent'], callable, job_params['global_var'], job_params['local_var'])
-
+            result['output'], result['success'] = _iterate(noised_simulation, r_setup['expected'], r_setup['guess'], r_setup['bounds'],
+                                        r_setup['variables'], r_setup['independent'], callable_, job_params['global_var'], job_params['local_var'])
             for key, value in fit_parameters.items():
-
                 frame.insert(0, value['columns'][count], result['output'][:, fit_parameters[key]['idx']])
+            frame.insert(0, '%s_success' % count, result['success'])
+
         for key, value in fit_parameters.items():
             mean = frame[value['columns']].iloc[:].mean(axis=1)
             stddev = frame[value['columns']].iloc[:].std(axis=1)
@@ -158,15 +161,15 @@ def parallel_sensi(wq):
         frame.insert(0, global_var['param'], buf)
         buf[0:len(setup['simulate'])] = setup['simulate']
         frame.insert(0, 'expected', buf)
-        frame.to_csv('results/two_variation_Deltaoutput_%s.txt' % job_params['global_var']['file_index'], index=False)
+        frame.to_csv('%stwo_variation_Deltaoutput_%s.txt' % (job_params['setup']['dir'], job_params['global_var']['file_index']), index=False)
         del model
         wq.task_done()
 
 
 def _iterate(simulation, expected, guess, bounds, variables, independent, callable_, global_, local_):
     biased_parameters = np.zeros((len(local_['input']), len(expected)))
+    success_ar = np.zeros(len(local_['input']))
     expected_dict = {key: value for key, value in zip(variables, expected)}
-
     gmod = Model(callable_, independent_vars=independent.keys(), param_names=variables, method='lbfgsb')
     for param, ini, limits in zip(variables, guess, bounds):
         gmod.set_param_hint(param, value=ini, min=limits[0], max=limits[1])
@@ -175,8 +178,9 @@ def _iterate(simulation, expected, guess, bounds, variables, independent, callab
     for i, input_ in enumerate(local_['input']):
         independent[local_['param']] = input_
         result = gmod.fit(simulation, verbose=False, **independent)
+        success_ar[i] = result.success
         biased_parameters[i] = np.array([result.params[key].value - expected_dict[key] for key in variables])
-    return biased_parameters
+    return biased_parameters, success_ar
 
 
 if __name__ == "__main__":
@@ -192,5 +196,7 @@ if __name__ == "__main__":
     logger.addHandler(ch)
     logger.setLevel(args.level)
     logger.info("Start")
-    startsky2D(logger)
-
+    model_param = get_model_param()
+    setup = fit_setup()
+    sensi = sensi_setup()
+    startsky2D(model_param, setup, sensi)
